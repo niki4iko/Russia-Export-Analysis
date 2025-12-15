d192b4b2f295774ea8a69418d1599ca4aa6dfd6c
niki4iko <epanchincevnikita3@gmail.com>
2025-12-16 02:24:07 +0700

    Cleaned up existing scripts, now using medalion arch
    Alligned the fileы content with  the general coding style, added some additional checks into 04_field_limits
    From now on project will follow medalion architecture, next commit should cointibute to README files explaining future architecture with documentation and diagrams. Having said that, taking a break from coding for now to figure out how to do mentioned tasks

--- SQL_ETL/03_init_raw_tables.sql
+++ SQL_ETL/03_init_raw_tables.sql
@@ -1,31 +1,36 @@
 /*
 The purpose of the script is just to load raw .csv data into the tables
 without any transformations, as this will happen later
+
+Currently works only on my machine
+(or if you place the repo exactly like in COPY)
+
+TODO: fix it on later stages with /COPY instructions and relative paths
 */
 
--- ================== stage.raw_partner_areas ==================
+-- ================== bronze.raw_partner_areas ==================
 -- Drop table if exists and create as new
-DROP TABLE IF EXISTS stage.raw_partner_areas;
-CREATE TABLE stage.raw_partner_areas (
+DROP TABLE IF EXISTS bronze.raw_partner_areas;
+CREATE TABLE bronze.raw_partner_areas (
     country_key  SMALLINT,
     country_name VARCHAR(50)
 );
 
 -- Load CSV into the table
-COPY stage.raw_partner_areas
+COPY bronze.raw_partner_areas
 FROM 'A:\Git_repos\Russia-Export-Analysis\data_raw\partnerAreas.csv'
 DELIMITER ','
 CSV HEADER;
 
 
 -- Preview first 10 rows
--- SELECT * FROM stage.raw_partner_areas LIMIT 10;
+-- SELECT * FROM bronze.raw_partner_areas LIMIT 10;
 
--- ================== stage.raw_iso3 ==================
-DROP TABLE IF EXISTS stage.raw_iso3;
+-- ================== bronze.raw_iso3 ==================
+DROP TABLE IF EXISTS bronze.raw_iso3;
 
-CREATE TABLE stage.raw_iso3 (
-    continent_name VARCHAR(10), -- 8 cap
+CREATE TABLE bronze.raw_iso3 (
+    continent_name VARCHAR(8), -- 8 cap
     region_name    VARCHAR(50), -- 17 cap
     country_name   VARCHAR(50), -- 33 cap
     capital_name   VARCHAR(50), -- 22 cap
@@ -38,30 +43,30 @@ CREATE TABLE stage.raw_iso3 (
 );
 
 
-COPY stage.raw_iso3
+COPY bronze.raw_iso3
 FROM 'A:\Git_repos\Russia-Export-Analysis\data_raw\iso3.csv'
 DELIMITER ','
 CSV HEADER;
 
 -- Preview first 10 rows
--- SELECT * FROM stage.raw_iso3 LIMIT 10;
+-- SELECT * FROM bronze.raw_iso3 LIMIT 10;
 
--- ================== stage.raw_rus_export ==================
-DROP TABLE IF EXISTS stage.raw_rus_export;
+-- ================== bronze.raw_rus_export ==================
+DROP TABLE IF EXISTS bronze.raw_rus_export;
 
-CREATE TABLE stage.raw_rus_export (
+CREATE TABLE bronze.raw_rus_export (
     index_key        INTEGER,
-    classification   VARCHAR(10), -- only S4 rows, will be dropped
+    classification   VARCHAR(10), -- only "S4" rows, will be dropped
     year_num         SMALLINT,
     aggregate_level  SMALLINT,
     is_leaf_code     BOOLEAN,
-    reporter_key     INTEGER,      -- Always 643, so cut
+    reporter_key     INTEGER,      -- Always "Russian Federation", so cut
     reporter_name    VARCHAR(100), -- same as above
     reporter_iso3    CHAR(3),      -- same as above
     partner_key      INTEGER,
     partner_name     VARCHAR(100),
     partner_iso3     CHAR(3),
-    commodity_key    VARCHAR(10), -- Either TOTAL, or INTEGER
+    commodity_code   VARCHAR(5),  -- SITC 4 classification code, 
     commodity_name   VARCHAR(500), 
     qty_unit_key     SMALLINT,
     qty_unit_name    VARCHAR(50),
@@ -70,10 +75,10 @@ CREATE TABLE stage.raw_rus_export (
     trade_value_usd  NUMERIC(18,2)
 );
 
-COPY stage.raw_rus_export
+COPY bronze.raw_rus_export
 FROM 'A:\Git_repos\Russia-Export-Analysis\data_raw\RUStoWorldTrade.csv'
 DELIMITER ','
 CSV HEADER;
 
 -- Preview first 1000 rows
--- SELECT * FROM stage.raw_rus_export WHERE commodity_key = '2308' LIMIT 1000;
+-- SELECT * FROM bronze.raw_rus_export LIMIT 1000;
--- SQL_ETL/04_field_limits.sql
+++ SQL_ETL/04_field_limits.sql
@@ -1,132 +1,177 @@
 /*
-Purpose: Profile raw tables to determine field limits (lengths, min/max values, null counts).
-This helps decide appropriate data types for warehouse tables.
+Purpose: Profile raw tables to determine field boundaries,
+check for NULLs, duplicates. No transformation should happen here.
+
+Currently considering it incomplete and somewhere messy
+The content of this file is still subject to change as my skills will grow
 */
 
 -- ========================= raw_partner_areas =========================
+-- Check for NULL-key and doubles
+SELECT
+	country_key,
+	COUNT(*) as row_total
+FROM bronze.raw_partner_areas
+GROUP BY country_key
+HAVING COUNT(*) > 1 
+
 -- Check string lengths and nulls
 SELECT 
-	COUNT(*) AS total_rows,
-	MIN(country_key) AS min_key,
-    MAX(country_key) AS max_key,
-	COUNT(country_key) AS non_null,
-    COUNT(*) - COUNT(country_key) AS nulls,
-    MIN(LENGTH(country_name)) AS min_len,
-    MAX(LENGTH(country_name)) AS max_len,
-    COUNT(country_name) AS non_null,
-    COUNT(*) - COUNT(country_name) AS nulls
-FROM stage.raw_partner_areas;
+	COUNT(*)                       AS total_rows,    -- 292
+	MIN(country_key)               AS min_key,       -- 4
+    MAX(country_key)               AS max_key,       -- 899
+	COUNT(country_key)             AS key_non_null,  -- 292
+    COUNT(*) - COUNT(country_key)  AS key_non_nulls, -- 0
+    MIN(LENGTH(country_name))      AS min_len,       -- 3
+    MAX(LENGTH(country_name))      AS max_len,       -- 44
+    COUNT(country_name)            AS name_non_null, -- 292
+    COUNT(*) - COUNT(country_name) AS name_nulls     -- 0
+FROM bronze.raw_partner_areas;
 
 -- ============================== raw_iso3 ==============================
 -- Profile text fields
 SELECT 
-    MAX(LENGTH(continent_name)) AS max_len_continent,
-    MAX(LENGTH(region_name)) AS max_len_region,
-    MAX(LENGTH(country_name)) AS max_len_country,
-    MAX(LENGTH(capital_name)) AS max_len_capital,
-    MAX(LENGTH(fips)) AS max_len_fips,
-    MAX(LENGTH(iso2)) AS max_len_iso2,
-    MAX(LENGTH(iso3)) AS max_len_iso3,
-    MAX(LENGTH(internet)) AS max_len_internet,
-    MAX(LENGTH(note)) AS max_len_note
-FROM stage.raw_iso3;
+    MAX(LENGTH(continent_name)) AS max_len_continent, -- 8
+    MAX(LENGTH(region_name))    AS max_len_region,    -- 17
+    MAX(LENGTH(country_name))   AS max_len_country,   -- 33
+    MAX(LENGTH(capital_name))   AS max_len_capital,   -- 22
+    MAX(LENGTH(fips))           AS max_len_fips,      -- 2
+    MAX(LENGTH(iso2))           AS max_len_iso2,      -- 2
+    MAX(LENGTH(iso3))           AS max_len_iso3,      -- 3
+    MAX(LENGTH(internet))       AS max_len_internet,  -- 5
+    MAX(LENGTH(note))           AS max_len_note       -- 1
+FROM bronze.raw_iso3;
 
 -- Profile numeric ISO key
 SELECT 
-    MIN(iso_key::INT) AS min_iso_key,
-    MAX(iso_key::INT) AS max_iso_key,
-    COUNT(*) AS total_rows,
-    COUNT(iso_key) AS non_null,
-    COUNT(*) - COUNT(iso_key) AS nulls
-FROM stage.raw_iso3
-WHERE iso_key ~ '^[0-9]+$';  -- only numeric values
+    MIN(iso_key::INT) AS min_iso_key,  -- 4
+    MAX(iso_key::INT) AS max_iso_key,  -- 894
+    COUNT(*) AS total_rows,            -- 226
+    COUNT(iso_key) AS non_null,        -- 226
+    COUNT(*) - COUNT(iso_key) AS nulls -- 0
+FROM bronze.raw_iso3
+WHERE iso_key ~ '^[0-9]+$'; -- only numeric values
 
 -- =========================== raw_rus_export ===========================
 -- Max string len
 SELECT 
-    MAX(LENGTH(classification))   AS max_len_classification, -- 2
-    MAX(LENGTH(reporter_name))    AS max_len_reporter_name, -- 18
-    MAX(LENGTH(reporter_iso3))    AS max_len_reporter_iso3, -- 3
-    MAX(LENGTH(partner_name))     AS max_len_partner_name, -- 36
-    MAX(LENGTH(partner_iso3))     AS max_len_partner_iso3, -- 3
-    MAX(LENGTH(commodity_key))    AS max_len_commodity_key, -- 5
-    MAX(LENGTH(commodity_name))   AS max_len_commodity_name, -- 411
-    MAX(LENGTH(qty_unit_name))    AS max_len_qty_unit_name -- 48
-FROM stage.raw_rus_export;
+    MAX(LENGTH(classification)) AS max_len_classification, -- 2
+    MAX(LENGTH(reporter_name))  AS max_len_reporter_name,  -- 18
+    MAX(LENGTH(reporter_iso3))  AS max_len_reporter_iso3,  -- 3
+    MAX(LENGTH(partner_name))   AS max_len_partner_name,   -- 36
+    MAX(LENGTH(partner_iso3))   AS max_len_partner_iso3,   -- 3
+    MAX(LENGTH(commodity_code)) AS max_len_commodity_code, -- 5
+    MAX(LENGTH(commodity_name)) AS max_len_commodity_name, -- 411
+    MAX(LENGTH(qty_unit_name))  AS max_len_qty_unit_name   -- 48
+FROM bronze.raw_rus_export;
 --  Numeric range profiling 
 SELECT 
     MIN(year_num)        AS min_year, -- 2007
     MAX(year_num)        AS max_year, -- 2020
     MIN(aggregate_level) AS min_aggregate_level, -- 0
     MAX(aggregate_level) AS max_aggregate_level, -- 5
-    MIN(qty_amount)      AS min_qty_amount, -- 0
-    MAX(qty_amount)      AS max_qty_amount, -- 75,265,886,900 
-    MIN(net_weight_kg)   AS min_net_weight, -- 0
-    MAX(net_weight_kg)   AS max_net_weight, -- 75,265,886,900
+    MIN(qty_amount)      AS min_qty_amount,  -- 0
+    MAX(qty_amount)      AS max_qty_amount,  -- 75,265,886,900 
+    MIN(net_weight_kg)   AS min_net_weight,  -- 0
+    MAX(net_weight_kg)   AS max_net_weight,  -- 75,265,886,900
     MIN(trade_value_usd) AS min_trade_value, -- 0.00
-    MAX(trade_value_usd) AS max_trade_value -- 66,607,214,414.00 
-FROM stage.raw_rus_export;
+    MAX(trade_value_usd) AS max_trade_value  -- 66,607,214,414.00 
+FROM bronze.raw_rus_export;
 -- Null counts
 SELECT 
-    COUNT(*) AS total_rows, -- 1,384,603
-    COUNT(classification)   AS non_null_classification, -- 1,384,603
-    COUNT(*) - COUNT(classification)   AS null_classification, -- 0
-    COUNT(reporter_name)    AS non_null_reporter_name, -- 1,384,603
-    COUNT(*) - COUNT(reporter_name)    AS null_reporter_name, -- 0
-    COUNT(partner_name)     AS non_null_partner_name, -- 1,384,603
-    COUNT(*) - COUNT(partner_name)     AS null_partner_name, -- 0
-    COUNT(commodity_key)    AS non_null_commodity_key, -- 1,384,603
-    COUNT(*) - COUNT(commodity_key)    AS null_commodity_key, -- 0
-    COUNT(commodity_name)   AS non_null_commodity_name, -- 1,384,603
-    COUNT(*) - COUNT(commodity_name)   AS null_commodity_name, -- 0
-    COUNT(qty_amount)       AS non_null_qty_amount, -- 1,283,894
-    COUNT(*) - COUNT(qty_amount)       AS null_qty_amount, -- 100,709
-	COUNT(qty_unit_key)     AS non_null_qty_units, -- -- 1,384,603
-	COUNT(*) - COUNT(qty_unit_key)       AS null_qty_units, -- 0
-    COUNT(net_weight_kg)    AS non_null_net_weight, -- 1,323,567
-    COUNT(*) - COUNT(net_weight_kg)    AS null_net_weight, -- 61,036
-    COUNT(trade_value_usd)  AS non_null_trade_value,  -- 1,384,603
-    COUNT(*) - COUNT(trade_value_usd)  AS null_trade_value -- 0
-FROM stage.raw_rus_export;
+    COUNT(*)                          AS total_rows, -- 1,384,603
+    COUNT(classification)             AS non_null_classification, -- 1,384,603
+    COUNT(*) - COUNT(classification)  AS null_classification,     -- 0
+    COUNT(reporter_name)              AS non_null_reporter_name,  -- 1,384,603
+    COUNT(*) - COUNT(reporter_name)   AS null_reporter_name,      -- 0
+    COUNT(partner_name)               AS non_null_partner_name,   -- 1,384,603
+    COUNT(*) - COUNT(partner_name)    AS null_partner_name,       -- 0
+    COUNT(commodity_code)             AS non_null_commodity_code, -- 1,384,603
+    COUNT(*) - COUNT(commodity_code)  AS null_commodity_code,     -- 0
+    COUNT(commodity_name)             AS non_null_commodity_name, -- 1,384,603
+    COUNT(*) - COUNT(commodity_name)  AS null_commodity_name,     -- 0
+    COUNT(qty_amount)                 AS non_null_qty_amount,     -- 1,283,894
+    COUNT(*) - COUNT(qty_amount)      AS null_qty_amount,         -- 100,709
+	COUNT(qty_unit_key)               AS non_null_qty_units,      -- 1,384,603
+	COUNT(*) - COUNT(qty_unit_key)    AS null_qty_units,          -- 0
+    COUNT(net_weight_kg)              AS non_null_net_weight,     -- 1,323,567
+    COUNT(*) - COUNT(net_weight_kg)   AS null_net_weight,         -- 61,036
+    COUNT(trade_value_usd)            AS non_null_trade_value,    -- 1,384,603
+    COUNT(*) - COUNT(trade_value_usd) AS null_trade_value         -- 0
+FROM bronze.raw_rus_export;
 -- Distinct counts
 SELECT 
-    COUNT(DISTINCT classification)   AS distinct_classification, -- 1
-    COUNT(DISTINCT year_num)         AS distinct_years, -- 14
-    COUNT(DISTINCT aggregate_level)  AS distinct_aggregate_levels, -- 6
-    COUNT(DISTINCT is_leaf_code)     AS distinct_leaf_flags, -- 2
-    COUNT(DISTINCT reporter_key)     AS distinct_reporters, -- 1
-    COUNT(DISTINCT reporter_name)    AS distinct_reporter_names, -- 1
-    COUNT(DISTINCT reporter_iso3)    AS distinct_reporter_iso3, -- 1
-    COUNT(DISTINCT partner_key)      AS distinct_partners, -- 225
-    COUNT(DISTINCT partner_name)     AS distinct_partner_names, -- 225
-    COUNT(DISTINCT partner_iso3)     AS distinct_partner_iso3, -- 222
-    COUNT(DISTINCT commodity_key)    AS distinct_commodity_keys, -- 3979
-    COUNT(DISTINCT commodity_name)   AS distinct_commodity_names, -- 3938
-    COUNT(DISTINCT qty_unit_key)     AS distinct_qty_units, -- 12
-    COUNT(DISTINCT qty_unit_name)    AS distinct_qty_unit_names -- 12
-FROM stage.raw_rus_export;
+    COUNT(DISTINCT classification)  AS distinct_classification,   -- 1
+    COUNT(DISTINCT year_num)        AS distinct_years,            -- 14
+    COUNT(DISTINCT aggregate_level) AS distinct_aggregate_levels, -- 6
+    COUNT(DISTINCT is_leaf_code)    AS distinct_leaf_flags,       -- 2
+    COUNT(DISTINCT reporter_key)    AS distinct_reporters,        -- 1
+    COUNT(DISTINCT reporter_name)   AS distinct_reporter_names,   -- 1
+    COUNT(DISTINCT reporter_iso3)   AS distinct_reporter_iso3,    -- 1
+    COUNT(DISTINCT partner_key)     AS distinct_partners,         -- 225
+    COUNT(DISTINCT partner_name)    AS distinct_partner_names,    -- 225
+    COUNT(DISTINCT partner_iso3)    AS distinct_partner_iso3,     -- 222
+    COUNT(DISTINCT commodity_code)  AS distinct_commodity_codes,  -- 3979
+    COUNT(DISTINCT commodity_name)  AS distinct_commodity_names,  -- 3938
+    COUNT(DISTINCT qty_unit_key)    AS distinct_qty_units,        -- 12
+    COUNT(DISTINCT qty_unit_name)   AS distinct_qty_unit_names    -- 12
+FROM bronze.raw_rus_export;
 -- Top values preview
-SELECT qty_unit_name, COUNT(*) AS row_count
-FROM stage.raw_rus_export
+SELECT qty_unit_name, COUNT(*) AS row_total
+FROM bronze.raw_rus_export
 GROUP BY qty_unit_name
-ORDER BY row_count DESC
-LIMIT 15; -- there is only 12 possible rows as distinct qty_units = qty_unit_names = 12
-
-SELECT
-	AVG(commodity_key::INT),
-	commodity_name, 
-	COUNT(*) AS row_total
-FROM stage.raw_rus_export
-GROUP BY commodity_name
 ORDER BY row_total DESC
-LIMIT 100;
+LIMIT 15; -- should be only 12 possible rows, cause distinct_qty_unit_names = 12
 
--- Where commodity_key is not an integer
-SELECT COUNT(*) -- 177, all "TOTAL"
-FROM stage.raw_rus_export
-WHERE NOT (commodity_key ~ '^[0-9]+$');
--- Where commodity_key is an integer
-SELECT COUNT(*) -- 1,384,426
-FROM stage.raw_rus_export
-WHERE (commodity_key ~ '^[0-9]+$');
--- total row count is 1,384,603 which is total row count in raw_rus_export
+
+
+/*
+All commodity_code data is under SITC 4 classification rules. It means
+If LEN(commodity_code) is as follows, then:
+1‑digit (Sections) → very broad categories (e.g., 0 = Food and live animals).
+2‑digit (Divisions) → narrower groups (e.g., 01 = Meat and meat preparations).
+3‑digit (Groups) → more detail (e.g., 011 = Meat of bovine animals).
+4‑digit (Subgroups) → finer detail (e.g., 0111 = Fresh/chilled bovine meat).
+5‑digit (Items) → the most detailed commodity codes
+Also in this dataset commodity_code may be TOTAL, 
+which means all kinds of commodities
+
+aggregate_level MUST always be equal to LEN(commodity_code),
+except cases where aggreagate_level is 0, then LEN(commodity_code) MUST be 5,
+the value MUST be "TOTAL" and is_leaf_code MUST be false
+
+If is_leaf_code is set, then LEN(commodity_code) ideally should be 5
+but there is a SITC 4 classifications that has max len of only 4, 
+	so i consider it ok data
+but if is_leaf_code == True and LEN(commodity_code) < 4, 
+	then considering it suspicious
+
+credits to Microsoft Copilot who helped me understand the concept
+
+Having that said, following code shows samples which does NOT qualify
+expected output is empty table
+*/
+SELECT
+	is_leaf_code AS leaf_summary,
+	aggregate_level,
+	LENGTH(commodity_code) AS code_length,
+	COUNT(commodity_code)  AS matching_commodities, 
+	COUNT(aggregate_level) AS matching_aggregate
+FROM bronze.raw_rus_export
+GROUP BY is_leaf_code, aggregate_level, LENGTH(commodity_code)
+HAVING NOT (
+	aggregate_level = LENGTH(commodity_code)
+	OR (aggregate_level = 0 AND LENGTH(commodity_code) = 5 AND is_leaf_code = FALSE)
+	OR (is_leaf_code = TRUE AND LENGTH(commodity_code) < 4)
+)
+ORDER BY is_leaf_code, aggregate_level, code_length
+
+/* Just to see the data sample
+SELECT
+	is_leaf_code,
+	commodity_code,
+	aggregate_level,
+	commodity_name
+FROM bronze.raw_rus_export
+LIMIT 100;
+*/
